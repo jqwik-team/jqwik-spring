d25463e57415891f4321510166877c43b0731311
Johannes Link <business@johanneslink.net>
2020-03-18 11:50:55 +0100

    Configured spring boot and (hopefully) everything else

--- src/main/java/net/jqwik/spring/JqwikSpringExtension.java
+++ src/main/java/net/jqwik/spring/JqwikSpringExtension.java
@@ -1,10 +1,16 @@
 package net.jqwik.spring;
 
+import net.jqwik.api.lifecycle.*;
 import org.apiguardian.api.*;
 
 /**
- * This class includes all the jqwik hooks necessary to use
+ * This class includes all the jqwik hooks necessary to use spring in examples and properties
  */
-@API(status = API.Status.INTERNAL, since = "0.1.0")
-public class JqwikSpringExtension {
+@API(status = API.Status.EXPERIMENTAL, since = "0.1.0")
+public class JqwikSpringExtension implements LifecycleHook {
+
+	@Override
+	public PropagationMode propagateTo() {
+		return PropagationMode.DIRECT_DESCENDANTS;
+	}
 }
